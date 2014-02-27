69afc9c5cf901d4e7968e59ee34328c1f7e5e957
Geoff Hulette <ghulette@gmail.com>
2014-02-26 16:00:21 -0800

    Update

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ weather
 
 Check the weather from the command line like magic! Also an excuse to
 play around with web service stuff in OCaml.
+
+To build you will need:
+* ocamlfind
+* Batteries 2.2
+* Ocamlnet 3.7.3
--- main.ml
+++ main.ml
@@ -1,9 +1,15 @@
 open Batteries
 open Printf
 
-let () =
-  let w = Weather.for_region "San Francisco, CA" in
-  let temp = Weather.temperature w |> Float.round_to_int in
-  let desc = Weather.description w in
-  printf "%d degrees, %s\n" temp desc;
-(*  printf "%s\n" (Weather.dump w)*)
+let rec loop () =
+  try 
+    let input = read_line () in
+    let w = Weather.for_region input in
+    let temp = Weather.temperature w |> Float.round_to_int in
+    let desc = Weather.description w in
+    printf "%d degrees - %s\n" temp desc;
+    loop ()
+  with
+  | End_of_file -> ()
+
+let () = loop ()
